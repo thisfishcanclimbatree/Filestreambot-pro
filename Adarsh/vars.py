98e5b5e026833c6de591ca3c39a0032ce71e8785
thisfishcanclimbatree <163320659+thisfishcanclimbatree@users.noreply.github.com>
2024-03-13 21:55:54 +0530

    Update vars.py

--- Adarsh/vars.py
+++ Adarsh/vars.py
@@ -36,5 +36,5 @@ class Var(object):
     else:
         URL = "http://{}/".format(FQDN)
     DATABASE_URL = str(getenv('DATABASE_URL', 'mongodb+srv://hogyesubs:1234567890@cluster0.lvayhk0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'))
-    UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', '-1001984636497'))
+    UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', 'pirated_courses_updates'))
     BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "-1001362659779")).split())) 
